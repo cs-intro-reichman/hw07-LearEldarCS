ddbbc29036f74138cee39fb957ef4962f3ed9d79
Lear Eldar <lear.eldar@post.runi.ac.il>
2025-12-30 21:34:27 +0200

    First Check - Koch

--- Koch.java
+++ Koch.java
@@ -8,7 +8,7 @@ public class Koch {
 		//// Uncomment only one block in each test, and remember to compile
 		//// the class whenever you change the test.
 
-        /*
+        /* 
 		// Tests the curve function:
 		// Gets n, x1, y1, x2, y2,
 		// and draws a Koch curve of depth n from (x1,y1) to (x2,y2).
@@ -17,18 +17,32 @@ public class Koch {
 		      Double.parseDouble(args[3]), Double.parseDouble(args[4]));
 		*/
 
-		/*
+		
 		// Tests the snowflake function:
 		// Gets n, and draws a Koch snowflake of n edges in the standard canvass.
 		snowFlake(Integer.parseInt(args[0]));
-		*/
+		
 	}
 
 	/** Gets n, x1, y1, x2, y2,
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
-		StdDraw.line(x1, y1, x2, y2);
 		//// Write the rest of your code below.
+		if (n == 0) {
+			StdDraw.line(x1,y1,x2,y2);
+			return;
+		}
+			double x12 = (2 * x1 + x2) / 3; double y12 = (2 * y1 + y2) / 3;
+			
+			double x21 = (x1 + 2 * x2) / 3; double y21 = (y1 + 2 * y2) / 3;
+			
+			double x3 = (Math.sqrt(3) / 6) * (y12 - y21) + (x12 + x21) / 2;
+			double y3 = (Math.sqrt(3) / 6) * (x21 - x12) + (y12 + y21) / 2;
+
+		curve(n-1, x1, y1, x12, y12);
+		curve(n-1, x12, y12, x3, y3);
+		curve(n-1, x3, y3, x21, y21);
+		curve(n-1, x21, y21, x2, y2);
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -38,5 +52,16 @@ public class Koch {
 		StdDraw.setXscale(0, 1.1);
 		// Draws a Koch snowflake of depth n
 		//// Write the rest of your code below.
+		
+		double x1 = 0.1; double y1 = 0.1;
+		double x2 = 1.0; double y2 = 0.1;
+		double xMid = (x1 + x2)/2; double yMid = (Math.sqrt(3)/2) * (x2 - x1);
+
+		curve(n, x1, y1, x2, y2);
+		curve(n, x2, y2, xMid, yMid);
+		curve(n, xMid, yMid, x1, y1);
+
+
+
 	}
 }
